bf2a0dc42ef210f5f83c60b9ddad0b010efc42c2
ZOUMATA Hasni <hasnizoumata@gmail.com>
2023-12-06 17:48:58 +0100

    gitignore setup

--- src/main/java/fr/uvsq/cprog/Directory.java
+++ src/main/java/fr/uvsq/cprog/Directory.java
@@ -1,11 +1,8 @@
 package fr.uvsq.cprog;
-
 import java.util.ArrayList;
 import java.util.List;
-
 public class Directory extends FileElement {
     private final List<FileElement> elements;
-
     public Directory(int NER, String name, String path) {
         super(NER, name, "directory", path);
         this.elements = new ArrayList<>();
