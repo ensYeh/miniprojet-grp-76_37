db3f030cd9769b8db076a0babda84785ebc22aa6
UVSQ22330626 <146948497+UVSQ22330626@users.noreply.github.com>
2023-12-13 12:27:29 +0100

    Test find + designe element

--- src/test/java/fr/uvsq/cprog/ConsoleManagerTest.java
+++ src/test/java/fr/uvsq/cprog/ConsoleManagerTest.java
@@ -1,5 +1,40 @@
 package fr.uvsq.cprog;
 
-public class ConsoleManagerTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ConsoleManagerTest {
+    String rootPath = ".\\RootTest";
+    ConsoleManager consoleManager = new ConsoleManager(rootPath);
+
+
+    @Test
+    void FindFile_Success() {
+        consoleManager.processCommand("find Settings.json");
+        int rootTestIndex = consoleManager.Output.indexOf("RootTest");
+        String outputPath = "";
+        if (rootTestIndex != -1) {
+            outputPath = consoleManager.Output.substring(rootTestIndex);
+        }
+        assertEquals("RootTest\\Zola\\Settings.json", outputPath);
+    }
+
+    @Test
+    void FindFile_NotFound() {
+        consoleManager.processCommand("find AZERTY");
+        assertEquals("File not found: AZERTY", consoleManager.Output);
+    }
+
+    @Test
+    void TestDesignateElement_Success() {
+        consoleManager.processCommand("3");
+        assertEquals("User designates element number 3: Topologie.PNG", consoleManager.Output);
+    }
+
+    @Test
+    void designateElement_NotFound() {
+        consoleManager.processCommand("55");
+        assertEquals("Element not found for NER 55", consoleManager.Output);
+    }
 }
